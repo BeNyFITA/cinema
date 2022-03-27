73ad701c47642dd551d2cb544ea7b3cf7cd0f0fd
BeNyFITA <davibnog@gmail.com>
2022-03-27 11:02:15 -0300

    Primeiro save no git do projeto...

---
+++ catalogo.js
@@ -0,0 +1,101 @@
+// Variavel Global dentro do código 
+var filmes = [          
+ {
+    id : 0,
+    title : "Bee Movie - A História de uma Abelha",
+    duracao : 1.31,
+    actors :JSON.stringify( ["Chris Rock", "Jhon Goodman"]),
+    anoDeLancamento : 2007,
+    emCartaz : false
+}
+];
+
+filmes.push(
+    {
+        id : 1,
+        title : "Spider Man - No Way Home",
+        duracao : 2.28,
+        actors :JSON.stringify(["Tom Holland", "Tobey Maguire", "Adndrew Garfield"]),
+        anoDeLancamento: 2021,
+        emCartaz  : true
+    }
+)
+/*
+A função deve receber os parâmetros com as informações necessárias para criar um objeto do tipo filme ;
+ */
+function adicionarFilmes(
+    id,
+    title,
+    duracao,
+    actors,
+    anoDeLancamento,
+    emCartaz
+
+){
+        filmes.push(
+            {
+                id,
+                title,
+                duracao,
+                actors,
+                anoDeLancamento,
+                emCartaz
+            }
+        )
+}
+/* 
+A função deve receber como parâmetro um número identificador do filme e buscar no array de catalogo e ao fim retornar o objeto encontrado;
+*/
+
+function buscarFilmes(identificador){
+let achou = null
+    for(let i = 0; i<filmes.length; i++ ){
+        if(filmes[i].id === identificador){
+            achou = filmes[i];
+            break;
+        }
+    }
+    console.log(achou);
+}
+
+
+function alterarStatusEmCartaz (identificador){
+    let filme = buscarFilmes(identificador)
+    filmes.emCartaz ===true ? false:true ;
+    return filme
+}
+
+
+
+
+adicionarFilmes(
+    2,
+    "O Retorno Do Jeji - Star Wars",
+    2.11,
+    JSON.stringify(["marck Hammil", "Carrier Fisher", "Harrison Ford"]),
+    1983,
+    false
+)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+module.exports = {
+    filmes,
+    adicionarFilmes,
+    buscarFilmes,
+    alterarStatusEmCartaz
+    
+};
+
---
+++ index.js
@@ -0,0 +1,7 @@
+const catalogo = require('./catalogo');
+
+catalogo.adicionarFilmes(3, "É o fim", 1.3,["Jhonan Hill", "Selena Gomez"], 2017, false);
+
+//catalogo.buscarFilmes(3);
+catalogo.alterarStatusEmCartaz(3);
+
