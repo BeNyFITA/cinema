76cb4b8491ccb64e4f2a0b5f664cecc616999295
BeNyFITA <davibnog@gmail.com>
2022-03-27 19:31:40 -0300

    Recebendo a execução do código que estava em catalogo.js!

--- index.js
+++ index.js
@@ -1,10 +1,26 @@
 const catalogo = require('./catalogo');
-
-catalogo.adicionarFilmes(3, "É o fim", 1.3,["Jhonan Hill", "Selena Gomez"], 2017, false);
+catalogo.adicionarFilmes(
+    2,
+    "O Retorno Do Jeji - Star Wars",
+    2.11,
+    JSON.stringify(["marck Hammil", "Carrier Fisher", "Harrison Ford"]),
+    1983,
+    false
+);
+catalogo.adicionarFilmes(
+    3,
+    "É o fim",
+    1.3,
+    ["Jhonan Hill", 
+    "Selena Gomez"], 
+    2017, 
+    false
+);
 
 catalogo.buscarFilmes(0);
 
 catalogo.alterarStatusEmCartaz(0);
+
 
 
 
